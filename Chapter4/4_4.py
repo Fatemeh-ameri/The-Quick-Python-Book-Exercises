c99aca7ef2bafc0f7c35cd9e7efd4cd56a5fe8a6
fatemehameri <fatemehamerizead@gmail.com>
2022-06-17 18:10:59 +0430

    variables and expressions

---
+++ Chapter4/4_4.py
@@ -0,0 +1,10 @@
+""" TRY THIS:variables and expressions: Use parentheses to group the
+numbers in different ways and see how the result changes compared with the
+original ungrouped expression."""
+
+x = 2 + 4 * 5 - 6 / 3
+x1 = (2 + 4) * (5 - 6) / 3
+x2 = (2 + 4) * (5- 6 / 3) 
+print(x)
+print(x1)
+print(x2)
