d41f2ecabb5cc516c337f428fb11adb3c256e0be
fatemehameri <fatemehamerizead@gmail.com>
2022-06-18 12:30:32 +0430

    quick check: pythonic style

---
+++ Chapter4/4_10.py
@@ -0,0 +1,30 @@
+"""Which of the following variable and function
+names do you think are not good Pythonic style? Why?"""
+
+
+#Function names:All lowercase, underscores_for_readablitiy
+
+#Variable names:All lowercase, underscores_for_readablitiy
+
+def bar():
+    pass
+#it is good because all condition for "function name" are done.
+
+def foo_bar():
+    pass
+#it is good because all condition for "function name" are done.
+
+varName = 0
+#it is not good because letter "N" is capital.
+
+VERYLONGVARNAME = 0
+#it is not good because all condition for "variable names" not done.
+
+foobar = 0
+#it is good because all condition for "variable name" are done.
+
+really_very_long_var_name = 0
+#it is good because all condition for "variable name" are done.
+
+longvarname = 0 
+#it is not good because all condition for "variable names" not done.
