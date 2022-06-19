b2968086b743ad8863f776742391a49597afd5c5
fatemehameri <fatemehamerizead@gmail.com>
2022-06-19 20:56:41 +0430

    some code added

--- Chapter4/4_4.py
+++ Chapter4/4_4.py
@@ -2,9 +2,22 @@
 numbers in different ways and see how the result changes compared with the
 original ungrouped expression."""
 
+
+#fatemeh ameri = 0
+#print(fatemeh ameri)
+#SyntaxError: invalid syntax
+#in variable name should not be a space
+
+#fatemeh-ameri = 0
+#print(fatemeh-ameri)
+#SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+
+fatemeh~ameri = 0
+print(fatemeh~ameri)
+#SyntaxError: invalid syntax
 x = 2 + 4 * 5 - 6 / 3
 x1 = (2 + 4) * (5 - 6) / 3
 x2 = (2 + 4) * (5- 6 / 3) 
-print(x)
-print(x1)
-print(x2)
+print("x = ",x)
+print("x1 = ",x1)
+print("x2 = ",x2)
