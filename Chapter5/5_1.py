b2b8d481430cf7e0e6fbf0729954e1c52e883773
fatemehameri <fatemehamerizead@gmail.com>
2022-06-27 20:48:09 +0430

    QUICK CHECK: LEN()

---
+++ Chapter5/5_1.py
@@ -0,0 +1,20 @@
+"""What would len() return for each of the following:
+[0]; []; [[1, 3, [4, 5], 6], 7]?"""
+
+
+
+
+
+
+from traceback import print_tb
+
+
+x= [0]
+print(len(x))
+
+x2= []
+print(len(x2))
+
+x3= [[1, 3, [4, 5], 6], 7]
+print(len(x3))
+
