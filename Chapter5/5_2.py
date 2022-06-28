05c92622cc3991b54655bc9ff7137d5d28d200dd
fatemehameri <fatemehamerizead@gmail.com>
2022-06-28 11:40:46 +0430

    LIST_SLICES_AND_INDEXES

---
+++ Chapter5/5_2.py
@@ -0,0 +1,8 @@
+"""Using what you know about the len()
+function and list slices, how would you combine the two to get the second half
+of a list when you don’t know what size it is? Experiment in the Python shell
+to confirm that your solution works."""
+
+x =[1,2,4,5,6,7,8]
+print(x[len(x)//2:])
+
