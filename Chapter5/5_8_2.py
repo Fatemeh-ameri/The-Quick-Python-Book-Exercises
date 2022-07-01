c73e6ee6bff22d6b73835fe83434a89bc70fd012
fatemehameri <fatemehamerizead@gmail.com>
2022-07-01 20:26:57 +0430

    QUICK CHECK: SETS

---
+++ Chapter5/5_8_2.py
@@ -0,0 +1,7 @@
+"""QUICK CHECK: SETS If you were to construct a set from the following list, how
+many elements would the set have?: [1, 2, 5, 1, 0, 2, 3, 1, 1, (1, 2, 3)]"""
+
+
+x = set([1, 2, 5, 1, 0, 2, 3, 1, 1, (1, 2, 3)])
+print(len(x))
+#output: 6
