6ce66bde2a0567c8a4215158596f6a387679b1ce
fatemehameri <fatemehamerizead@gmail.com>
2022-07-14 18:03:09 +0430

    QUICK CHECK: SPLIT AND JOIN

---
+++ Chapter6/6_4_1.py
@@ -0,0 +1,9 @@
+"""QUICK CHECK: SPLIT AND JOIN How could you use split and join to change
+all the whitespace in string x to dashes, such as changing "this is a test"
+to "this-is-a-test"?"""
+
+x = "this is a test"
+y = x.split()
+print(y)
+z = "-".join(y)
+print(z)
