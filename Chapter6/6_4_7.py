6065c36a69f15530774e25c0be2f72fdff1e697a
fatemehameri <fatemehamerizead@gmail.com>
2022-07-22 21:55:21 +0430

    TRY THIS: STRING OPERATIONS

---
+++ Chapter6/6_4_7.py
@@ -0,0 +1,24 @@
+"""TRY THIS: STRING OPERATIONS Suppose that you have a list of strings in which
+some (but not necessarily all) of the strings begin and end with the double
+quote character:
+x = ['"abc"', 'def', '"ghi"', '"klm"', 'nop']Converting from objects to strings
+What code would you use on each element to remove just the double quotes?
+What code could you use to find the position of the last p in Mississippi?
+When you’ve found that position, what code would you use to remove just
+that letter?"""
+
+import re
+x = ['"abc"', 'def', '"ghi"', '"klm"', 'nop']
+for i in range(len(x)):
+    if '"' in x[i]:
+        x[i] = x[i].strip('"')  
+print(x)
+#output: ['abc', 'def', 'ghi', 'klm', 'nop']
+
+y = "Mississippi"
+print(y.rindex("p"))
+#output: 9
+
+y1 = y.rfind("p")
+print(y[:y1] + y[y1+1:])
+#output: Mississipi
