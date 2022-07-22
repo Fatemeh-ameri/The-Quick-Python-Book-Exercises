24b75e49b2fe68b1200f490de5f8341de8274c8b
fatemehameri <fatemehamerizead@gmail.com>
2022-07-22 11:07:36 +0430

    QUICK CHECK: STRING SEARCHING

---
+++ Chapter6/6_4_4.py
@@ -0,0 +1,9 @@
+""" QUICK CHECK: STRING SEARCHING If you wanted to check whether a line ends
+with the string "rejected", what string method would you use? Would there
+be any other ways to get the same result?"""
+
+x = "she is rejected"
+
+print(x.endswith("rejected"))
+#output: True
+
