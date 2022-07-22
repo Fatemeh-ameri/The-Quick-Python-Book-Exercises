1b867767ec8de7a3c3787611c3fc65562cdc3d21
fatemehameri <fatemehamerizead@gmail.com>
2022-07-22 13:47:04 +0430

    QUICK CHECK: MODIFYING STRINGS

---
+++ Chapter6/6_4_6.py
@@ -0,0 +1,12 @@
+"""QUICK CHECK: MODIFYING STRINGS What would be a quick way to change all
+punctuation in a string to spaces?"""
+
+import string
+
+
+x = "I am fateme, I am 26 years old! Am I married? yes."
+punc = string.punctuation
+x2 = x.maketrans(string.punctuation, " " *len(string.punctuation))
+x3 = x.translate(x2)
+
+print(x3)
