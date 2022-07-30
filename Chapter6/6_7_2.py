5edfd1e68e9bd8dc16d562468df6c6e7cdb2fec5
fatemehameri <fatemehamerizead@gmail.com>
2022-07-30 15:25:43 +0430

    QUICK CHECK: FORMATTING STRING WITH

--- Chapter6/6_7_2.py
+++ Chapter6/6_7_2.py
@@ -1,4 +1,5 @@
-"""What would be in the variable x
+"""QUICK CHECK: FORMATTING STRING WITH 
+What would be in the variable x
 after the following snippets of code have executed?
 QUICK CHECK: FORMATTING STRINGS WITH %
 x = "%.2f" % 1.1111
@@ -7,9 +8,12 @@ x = "%(a).08f" % {'a':1.1111}"""
 
 x = "%.2f" % 1.1111
 print(x)
+#output: 1.11
 
 x = "%(a).2f" % {'a':1.1111}
 print(x)
+#output: 1.11
 
 x = "%(a).08f" % {'a':1.1111}
 print(x)
+#output: 1.11110000
