021e0e225e17fdcb98f01b7e26f860fc29deec76
fatemehameri <fatemehamerizead@gmail.com>
2022-07-31 16:37:21 +0430

    PREPROCESSING TEXT

---
+++ Chapter6/LAB6.py
@@ -0,0 +1,19 @@
+import re
+import string
+
+with open("moby_01.txt") as infile, open("moby_01_clean.txt", "w") as outfile:
+    for line in infile:
+        # make all one case
+        line=line.lower()
+
+        # remove punctuation
+        line = re.sub(r'[^\w\s]', ' ', line)
+        #line1 = line.translate(str.maketrans('', '', string.punctuation))
+
+        # split into words
+        line = line.split()
+
+        # write all words for line
+        line = "\n".join(line)
+        print(line)
+        outfile.write(line)
