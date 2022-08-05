108aa5ef15e5bc418906dc0abbb6a9c22e926187
fatemehameri <fatemehamerizead@gmail.com>
2022-08-05 11:04:07 +0430

    TRY THIS: LIST SLICES AND INDEXES

--- Chapter5/5_2.py
+++ Chapter5/5_2.py
@@ -1,4 +1,4 @@
-"""Using what you know about the len()
+"""TRY THIS: LIST SLICES AND INDEXES Using what you know about the len()
 function and list slices, how would you combine the two to get the second half
 of a list when you don’t know what size it is? Experiment in the Python shell
 to confirm that your solution works."""
@@ -6,4 +6,4 @@ to confirm that your solution works."""
 x =[1,2,4,5,6,7,8]
 print(x[len(x)//2:])
 #output: [5, 6, 7, 8]
-
+#slice indices must be integers
