e56816181bf60d879917afa7e782285b30dd63f3
fatemehameri <fatemehamerizead@gmail.com>
2022-08-07 16:43:52 +0430

    TRY THIS: MODIFYING LISTS

--- Chapter5/5_3.py
+++ Chapter5/5_3.py
@@ -3,11 +3,11 @@ might you move the last three items from the end of the list to the beginning,
 keeping them in the same order?"""
 
 x = [1,2,3,4,5,6,7,8,9,10]
+x1 = x[-3:]
+print(x1)
+#output: [8, 9, 10]
 
-del x[7:10]
-print(x)
-#output: [1, 2, 3, 4, 5, 6, 7]
-
-x [:0] = [8,9,10]
-print(x)
+del x[-3:]
+x1.extend(x)
+print(x1)
 #output: [8, 9, 10, 1, 2, 3, 4, 5, 6, 7]
