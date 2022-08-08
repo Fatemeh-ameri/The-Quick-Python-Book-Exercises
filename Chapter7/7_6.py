d356351a4207fb2416c4abf1c10e074724711e28
fatemehameri <fatemehamerizead@gmail.com>
2022-08-08 22:22:32 +0430

    TRY THIS: USING DICTIONARIES

---
+++ Chapter7/7_6.py
@@ -0,0 +1,25 @@
+"""TRY THIS: USING DICTIONARIES Suppose that you’re writing a program that
+works like a spreadsheet. How might you use a dictionary to store the con-
+tents of a sheet? Write some sample code to both store a value and retrieve a
+value in a particular cell. What might be some drawbacks to this approach?"""
+
+
+sheet = {}
+sheet[(1,1)] = input("Please Enter a number: ")
+sheet[(1,2)] = input("Please Enter a number: ")
+
+print(sheet[(1,1)])
+#output: 10
+
+   
+
+
+
+
+
+
+
+
+
+
+ 
