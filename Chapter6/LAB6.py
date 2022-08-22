c79ef7fe50e63a426dc956cdc91d24cd19251d38
fatemehameri <fatemehamerizead@gmail.com>
2022-08-22 12:01:17 +0430

    PREPROCESSING TEXT

--- Chapter6/LAB6.py
+++ Chapter6/LAB6.py
@@ -1,3 +1,22 @@
+"""LAB 6: PREPROCESSING TEXT In processing raw text, it’s quite often necessary
+to clean and normalize the text before doing anything else. If you want to
+find the frequency of words in text, for example, you can make the job easier
+if, before you start counting, you make sure that everything is lowercase (or
+uppercase, if you prefer) and that all punctuation has been removed. You can
+also make things easier by breaking the text into a series of words. In this lab,
+the task is to read the first part of the first chapter of Moby Dick (found in the
+book's source code), make sure that everything is one case, remove all punc-
+tuation, and write the words one per line to a second file. Because I haven’t
+yet covered reading and writing files, here’s the code for those operations:
+with open("moby_01.txt") as infile, open("moby_01_clean.txt", "w") as outfile:
+for line in infile:
+# make all one case
+# remove punctuation
+# split into words
+# write all words for line
+outfile.write(cleaned_words)"""
+
+
 import re
 import string
 
