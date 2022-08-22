f336c563ad012524a987454bf34b2a340f96e9ae
fatemehameri <fatemehamerizead@gmail.com>
2022-08-22 11:55:52 +0430

    QUICK CHECK: BYTES

--- Chapter6/6_9.py
+++ Chapter6/6_9.py
@@ -2,8 +2,6 @@
 to use a string? For which could you use bytes?
 1 Data file storing binary data
 2 Text in a language with accented characters
-CHAPTER 6
-Strings
 3 Text with only uppercase and lowercase roman characters
 4 A series of integers no larger than 255"""
 
